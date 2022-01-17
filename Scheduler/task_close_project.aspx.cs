dfde4d4a032b46e9035e14742c63e9f0232ab5c0
ST-NUM <62989134+ST-NUM@users.noreply.github.com>
2022-01-17 15:47:27 +0700

    Add files via upload

---
+++ Scheduler/task_close_project.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class task_close_project : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            CommonFunction.SetLog(0, "", "Run task Close Project");
+
+            CloseProject();
+        }
+        catch (Exception ex)
+        {
+            CommonFunction.SetLog(0, "", "Task Close Project Error " + ex.Message);
+        }
+        finally
+        {
+            ScriptManager.RegisterStartupScript(Page, GetType(), "JsStatus", "window.opener = 'Self';window.open('','_parent',''); window.close();", true);
+        }
+    }
+
+
+    public static void CloseProject()
+    {
+        var dNow = DateTime.Now.Date;
+        var dClose = new DateTime(dNow.Year, 1, 1);
+        if (dNow == dClose)
+        {
+            PTTGC_CSREntities db = new PTTGC_CSREntities();
+            int nYearClose = dNow.Year - 1;
+            var lstProject = db.T_Project.Where(w => !w.IsDel && w.IsPassApprove && w.nStatusID == 2 && w.nYear == nYearClose).ToList();
+            if (lstProject.Any())
+            {
+                foreach (var item in lstProject)
+                {
+                    item.nStatusID = 5;
+
+                    db.T_Project_Approve.Add(new T_Project_Approve()
+                    {
+                        nProjectID = item.nProjectID,
+                        nStatusID = 5,
+                        sComment = "ปิดโครงการโดยระบบ",
+                        nActionBy = null,
+                        dAction = DateTime.Now
+                    });
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Scheduler/task_update_emp.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class task_update_emp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            CommonFunction.SetLog(0, "", "Run task Update Employee");
+
+            UpdateEmployee();
+        }
+        catch (Exception ex)
+        {
+            CommonFunction.SetLog(0, "", "Task Update Employee Error " + ex.Message);
+        }
+        finally
+        {
+            ScriptManager.RegisterStartupScript(Page, GetType(), "JsStatus", "window.opener = 'Self';window.open('','_parent',''); window.close();", true);
+        }
+    }
+
+
+    public static void UpdateEmployee()
+    {
+        PTTGC_CSREntities db = new PTTGC_CSREntities();
+
+        var lstEmp = db.TB_User.Where(w => w.IsGC && !w.IsDel).ToList();
+        if (lstEmp.Any())
+        {
+            foreach (var item in lstEmp)
+            {
+                var qEmp_ = HR_WebService.EmployeeService_Search(item.sUserID, "", new List<string>()).d.results.FirstOrDefault();
+                if (qEmp_ != null)
+                {
+                    item.sFirstname = qEmp_.NameTH.Split(' ')[0] + " " + qEmp_.THFirstName;
+                    item.sLastname = qEmp_.THLastName;
+                    item.sEmail = qEmp_.EmailAddress;
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
